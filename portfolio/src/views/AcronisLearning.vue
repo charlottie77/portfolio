23d4534136976f09eb2d8771e6ff1e4b17189bac
yuqing.guo <charlottie77@163.com>
2020-08-18 01:46:40 -0400

    update

--- portfolio/src/views/AcronisLearning.vue
+++ portfolio/src/views/AcronisLearning.vue
@@ -1,136 +1,65 @@
 <template>
-  <div id='smart-node'>
+  <div id='acronis-learning'>
     <Banner height="32.2rem" backgroundColor="#DBE5FD"/>
     <div class='container' id='container-1'>
-      <img :src='require("@/assets/img/sn_illu.svg")' alt="" id="phone-icon">
+      <img :src='require("@/assets/img/al_title_illu.png")' alt="" id="title-mock">
       <div id="brief-description">
         <div id="project-title">Acronis Learning</div>
-        <div id="project-summary">A web programming tool for coding novices to build open-source hardware maker projects in a visual and low-coding way.</div>
+        <div id="project-summary">A learning platform that delivers hands-on modules with customized learning paths.</div>
       </div>
+      <div id="intro">
+        <div id="intro-text"> 
+          <div id="question"><img :src='require("@/assets/img/al_q_bullet.svg")' alt="" id="bullet"><span>How might we improve efficiency & learner engagement in Acronis software training?</span></div>
+          <p>In this capstone project, I worked as a product designer for Acronis, a global technology company that delivers data protection and disaster recovery services for virtual, physical, mobile, and cloud environments. </p>
+          <p>Our team's goal is to improve efficiency & learner engagement for Acronis Certified Engineer Training. The ACE training enables administrators to manage their IT infrastructures better and ultimately save costs by providing guidance on project implementation, rollout, and troubleshooting for channel partners and end-users. </p>
+        </div>
+        <div id="intro-pic">
+          <img :src='require("@/assets/img/acronis_backup_logo.png")' alt="" />
+          <img :src='require("@/assets/img/acronis_academy_logo.png")' alt="" style="margin-top: 3.4rem;"/>
+        </div>
+
+      </div>
+
+      <img :src='require("@/assets/img/al-splitter.svg")' alt="" class="img-center" id="splitter-1">
+
       <div id="my-role">
-        <div id="my-role-title">My Role</div>
-        <div id='my-role-detail' class="row">
-          <div id="my-role-1" class="col-12">
-            <p><span class="bold">Front-end Development @ IfCreate</span></p>
-          </div>
-          <img id="my-role-2" :src='require("@/assets/img/sn_role_pic.png")'>
-          <div id="my-role-3">
-            <p> I am responsible for the <span class="bold-green "> front-end implementation of  “Grammar Section” module </span> which includes 10 components, such as the Logic Patch, Control Flow Patch, Operator Patch, Math Patch, and Unititly Patch. I <span class="bold-green "> attuned the code with designers to deliver the pixel-perfect experience </span> in both interactive and visual aspects. </p>
-            <p id="mr3-1">Duration:  4 months </p>
-            <p>Collaborators: Evan Feng (Designer), Zenan Sun, Jay Zhu</p>
-          </div>
+        <div id="mr-title">My Role</div>
+        <div id="mr-overview">
+            <ul>
+              <li>Product Designer & Manager</li>
+              <li>Learning Experience Designer</li>
+            </ul>
+          <p>I was responsible for the product design work and mainly focused on designing the learner side experience, from concepts to visual execution. I also consolidated and built the design system. </p>
         </div>
-      </div>
-      <div id="demo">
-         <div id="demo-title">Online Demo (Chinese)</div>
-         <div id="demo-link">
-           <a target="_blank" rel="noopener" href="https://dognan.gitee.io/test/#/playground/1"><p>https://dognan.gitee.io/test/#/playground/1 </p></a>
-         </div>
-      </div>
-     
-       <div class="row" id='sn-mock-line-1' >
-        <img :src='require("@/assets/img/sn_mock_s_1.png")'>
-        <img :src='require("@/assets/img/sn_mock_s_2.png")'>
-        <img :src='require("@/assets/img/sn_mock_s_3.png")'>
-      </div>
-
-      <img :src='require("@/assets/img/sn-splitter.svg")' alt="" class="img-center" id="splitter-1">
-
-      <div id="final-design-1">
-        <div id="fd1-title">Final Product</div>
-        <p>In the maker learning process, the novices, such as primary students and designers,  always spend a lot of time on the line-by-line coding or debugging but feel frustrated because they are lack of programming knowledge. Thus, we designed and built SmartNode to provide development aids. Thus, the problem statement is:</p>
-        <div id="fd1-ps">
-           <img  :src='require("@/assets/img/sn_ps.svg")' />
-        </div>
-        <p>Using SmartNode, by dragging, dropping and connecting the nodes, novices can do programming for their maker project in a visual and “light on coding” way. The learning with SmartNode is clearer and more straightforward. Students can do technical innovation with lower barrier. </p>
-        <div id="fd1-curated-list">
-          <div class="fd1-cl-item">
-            <div class="fd1-cli-title">
-              <span>1</span>
-              <label>Login & Browse Project</label>
+        <div id="mr-sub4">
+          <div class="mrs-row">
+            <div class="mrs-item">
+              <label>INSIGHTS & IDEATION</label>
+              <p>I collaborated with one researcher to conduct 15+ contextual interviews, six speed-dating, three rounds of usability testings to uncover the insights. I translated needs into concepts & features with one designer.  </p>
             </div>
-            <img class="fd1-cli-gif" :src='require("@/assets/img/sn_final_1.gif")'/>
-          </div>
-          <div class="fd1-cl-item">
-            <div class="fd1-cli-title">
-              <span>2</span>
-              <label>Maker Toolbox</label>
+            <div class="mrs-item">
+              <label>INSIGHTS & IDEATION</label>
+              <p>I collaborated with one researcher to conduct 15+ contextual interviews, six speed-dating, three rounds of usability testings to uncover the insights. I translated needs into concepts & features with one designer.  </p>
             </div>
-            <img class="fd1-cli-gif" :src='require("@/assets/img/sn_final_2.gif")'/>
-            <p>With the toolbox, student can browse the connected hardware, add new hardware, and use grammar blocks for advanced programming.</p>
           </div>
-          <div class="fd1-cl-item">
-            <div class="fd1-cli-title">
-              <span>3</span>
-              <label>Visual Programming</label>
+          <div class="mrs-row">
+            <div class="mrs-item">
+              <label>INSIGHTS & IDEATION</label>
+              <p>I collaborated with one researcher to conduct 15+ contextual interviews, six speed-dating, three rounds of usability testings to uncover the insights. I translated needs into concepts & features with one designer.  </p>
             </div>
-            <img class="fd1-cli-gif" :src='require("@/assets/img/sn_final_3.gif")'/>
-            <p>Students can do visual programming by dragging, dropping and connecting the hardware/grammer nodes in the canvas. </p>
-          </div>
-          <div class="fd1-cl-item">
-            <div class="fd1-cli-title">
-              <span>4</span>
-              <label>Test & Debug</label>
+            <div class="mrs-item">
+              <label>INSIGHTS & IDEATION</label>
+              <p>I collaborated with one researcher to conduct 15+ contextual interviews, six speed-dating, three rounds of usability testings to uncover the insights. I translated needs into concepts & features with one designer.  </p>
             </div>
-            <img class="fd1-cli-gif" :src='require("@/assets/img/sn_final_4.gif")'/>
-            <p>After building a complete flow, students can click “test” to see the values flowing.</p>
-          </div>
-
-        </div>
-      </div>
-
-      <div id="background">
-        <div id="background-title">Background</div>
-        <div class="background-sub-title">Maker Education & Open-Sourced Hardware</div>
-        <p>Building on the work of educational theorists like <a class="green-text" target="_blank" rel="noopener" href="https://en.wikipedia.org/wiki/Jean_Piaget"><span>Jean Piaget (constructivism)</span></a> and Seymour Papert (constructionism) maker-centered learning develops that awareness through interactive, open-ended, student-driven, multi-disciplinary experiences that allow for the time and space needed to develop diverse skills, knowledge, and ways of thinking. In maker-centered learning environments, students imagine, design, and create projects that align the content of learning with hands-on application.</p>
-        <div class="bg-duo-pic">
-          <div class="bgd-item">
-            <img :src='require("@/assets/img/sn_pic_1.png")' />
-            <label>from Unsplash</label>
-          </div>
-          <div class="bgd-item">
-            <img :src='require("@/assets/img/sn_pic_2.png")' />
-            <label>from Unsplash</label>
           </div>
         </div>
-        <p>Open-source hardware consists of physical artifacts of technology designed and offered by the open-design movement and it is becoming increasingly popular due to some nice but low-cost technologies, such as Raspberry Pi, micro-controllers(e.g. Arduino), 3D-printers, sensors, and actuators. </p>
-        <p>Thus, open-source hardware is a good resource that students can use for the technical innovation in maker education. For example, students can use Arduino Uno to build a <a target="_blank" rel="noopener" href="https://maker.pro/arduino/projects/ice-cube-detection-system-in-the-refrigerator"> <span class="green-text">ice cube detection system.  </span> </a> </p>
-        <div class="bg-duo-pic">
-          <div class="bgd-item">
-            <img :src='require("@/assets/img/sn_pic_3.png")' />
-            <label>from Unsplash</label>
-          </div>
-          <div class="bgd-item">
-            <img :src='require("@/assets/img/sn_pic_4.png")' />
-            <label>from Unsplash</label>
-          </div>
-        </div>
-         <div class="background-sub-title" id="bgst-2" >Our Goal</div>
-         <p>IfCreate aims to create better Maker Education for K-12 in China. SmartNode is designed for for the coding novice, such as primary students, middle school students, and designers, who are lack of programming knowledge. The goal of Maker Education is more about design, and innovation that align the content of learning with hands-on application.</p>
-         <p>However, students always spend a lot of time on the line-by-line coding or debugging and feel frustrated by doing that. Thus, we designed and built SmartNode to provide development aids. </p>
-         <p>By dragging, dropping and connecting the nodes, they can do programming for their maker project in a visual and “light on coding” way. The learning with SmartNode is clearer and more straightforward. Students can do technical innovation with lower barrier. </p>
-         <div class="background-sub-title" id="bgst-3" >How visual development aids can help? </div>
-         <div id="visual-aids">
-            <img :src='require("@/assets/img/sn_visual_aids.png")' />
-         </div>
-        
+
       </div>
 
-      <div id="demo" style="margin-bottom:6.4rem;">
-        <div id="d-title">Final Product</div>
-         <div id="demo-title">Online Demo (Chinese)</div>
-         <div id="demo-link">
-           <a target="_blank" rel="noopener" href="https://dognan.gitee.io/test/#/playground/1"><p>https://dognan.gitee.io/test/#/playground/1 </p></a>
-         </div>
-      </div>
-       <div class="row" id='sn-mock-line-1' style="margin-bottom:22rem;">
-        <img :src='require("@/assets/img/sn_mock_s_1.png")'>
-        <img :src='require("@/assets/img/sn_mock_s_2.png")'>
-        <img :src='require("@/assets/img/sn_mock_s_3.png")'>
-      </div>
+      
      
     </div>
-    <OnTopTools themeColor='#41988E' heightToShow='332' :navOnly='false' activeSpan='work'/>
+    <OnTopTools themeColor='#0D44CC' heightToShow='332' :navOnly='false' activeSpan='work'/>
   </div>
 </template>
 
@@ -182,10 +111,15 @@ export default {
   color: #41988E;
 }
 
-#smart-node{
+#acronis-learning{
   width: 100%;
   #Banner { position: absolute; width:100%}
-  #phone-icon{ position: absolute; right:8rem; top: 5rem;}
+  #title-mock{ 
+    position: absolute; 
+    width: 37.6rem;
+    right:0rem; 
+    top: 5rem;
+    }
   .container {
     position: relative;
     max-width: 85.7rem;
@@ -217,218 +151,99 @@ export default {
         font-size: 2rem;
         line-height: 2.8rem;
         letter-spacing: 0.04rem;
-        min-width: 50rem;
+        min-width: 45rem;
         color: #A6A6A6;
       }
     }
-    #question{
-        position: relative;
-        margin-top: 3.5rem;
-        width: 70%;
-        #bullet{
-          position: absolute;
-          left: -2.2rem;
-          top: -0.5rem;
-        }
-        span{
+    #intro {
+      display: flex;
+      #intro-text {
+        flex: 1.1;
+        #question{
           position: relative;
-          font-family: 'Ubuntu-M';
-          font-size: 2.2rem;
-          line-height: 2.8rem;
-          letter-spacing: 0.48125px;
-          color: #57A79E;
+          margin-top: 3.5rem;
+          margin-bottom: 3.6rem;
+          #bullet{
+            position: absolute;
+            left: -2.2rem;
+            top: -0.5rem;
+          }
+          span{
+            position: relative;
+            font-family: 'Ubuntu-M';
+            font-size: 2.2rem;
+            line-height: 2.8rem;
+            letter-spacing: 0.48125px;
+            color: #0D44CC;
+          }
+        }
+        p{
+          margin-top: 1.2rem;
         }
       }
-    #sn-mock-line-1 {
-      margin-top: 3.6rem;
-      position: relative;
-      left: calc( 50% - 55.25rem);
-      margin-left: auto;
-      margin-right: auto;
-      img {
-        width: 34.2rem;
-      }
-    }
-    #my-role{
-      margin-top: 9rem;
-      #my-role-title{
-        font-family: 'Ubuntu-Bold';
-        font-size: 2.4rem;
-        line-height: 2.8rem;
-        letter-spacing: 0.75px;
-        color: #41988E;
-      }
-      #my-role-detail{
-        margin-top: 1.1rem;
+      #intro-pic {
+        flex: 1;
         display: flex;
         flex-direction: column;
-        align-items: center;
-        #my-role-1{
-          padding: 0rem 2rem 0rem 0rem;
-          max-width: 43.1rem;
-          align-self: flex-start;
-          margin-top: 0.8rem;
-          p:nth-child(2){
-            margin-top: 1.5rem;
-          }
-        }
-      }
-      #my-role-2 {
-        width: 52.4rem;
-        margin-top: 3.6rem;
-      }
-      #my-role-3 {
-        margin-top: 3.6rem;
-        #mr3-1{
-          margin-top: 2.4rem;
-        }
-      }
-    }
-    #demo{
-      margin-top: 16rem;
-      #d-title{
-        @include gray-title();
-        margin-bottom: 10rem;;
-      }
-      #demo-title {
-        @include black-subtitle();
-      }
-      #demo-link {
-        margin-top: 3.6rem;
-        padding: 1.6rem 5.2rem;
-        
-        a{
-          background-color: #F2F8F7;
-          display: flex;
-          justify-content: center;
-          padding: 1.6rem 0rem;
-          border-radius: 0.6rem;
-          text-decoration-color: #41988E;
-          p{
-            font-size: 2rem;
-            font-family: 'Open Sans-SB';
-            color: #41988E;
-          }
-        }
-        
-      }
-    }
-    #final-design-1 {
-      margin-top: 16rem;
-      #fd1-title {
-        @include gray-title();
-        margin-bottom: 3.6rem;
-      }
-      #fd1-ps {
-        margin-top: 4.6rem;
-        margin-bottom: 4.6rem;
-        display: flex;
         justify-content: center;
-      }
-      #fd1-curated-list{
-        margin-top: 6.4rem;;
-      }
-      #fd1-curated-list {
-        .fd1-cl-item{
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          margin-bottom: 10rem;
-          .fd1-cli-title{
-            display: flex;
-            span{
-              width: 2.6rem;
-              height: 2.6rem;
-              background: #41988E;
-              border-radius: 1.3rem;
-
-              font-family: 'Open Sans-B';
-              font-size: 1.8rem;
-              line-height: 2.3rem;
-              display: flex;
-              align-items: center;
-              justify-content: center;
-              letter-spacing: 0.4px;
-              color: #FFFFFF;
-              margin-right: 1.2rem;
-            }
-            label{
-              font-family: 'Open Sans-BI';
-              font-size: 2.6rem;
-              line-height: 3rem;
-              letter-spacing: 0.577778px;
-              color: #41988E;
-            }
-          }
-          p{
-            width: 80%;
-          }
-          .fd1-cli-gif{
-            margin-top: 2.4rem;
-            margin-bottom: 2.4rem;
-            width: 85%;
-            box-shadow: 2px 2px 12px #D1D1D1;
-          }
+        align-items: center;
+        img {
+          width: 60%;
+          
         }
       }
-
+      
     }
-    
     #splitter-1{
       margin-top: 13.9rem;
     }
-    #background{
-      p{
-        margin-top: 1.2rem;
-        a{
-          text-decoration: none;
-        }
-      }
-      #background-title{
-        @include gray-title();
-        margin-top: 13.8rem;
-        margin-bottom:4.8rem;
-      }
-      .background-sub-title {
-        @include black-subtitle();
-        margin-bottom: 2.4rem;
-      }
-      #bgst-2{
-        margin-top: 14.4rem;
-      }
-      #bgst-3 {
-         margin-top: 14.4rem;
-      }
-      #visual-aids{
-        display: flex;
-        justify-content: center;
-        img{
-          width: 70%;
-        }
 
+    #my-role {
+      margin-top: 10rem;
+      #mr-title {
+        @include gray-title();
       }
-      .bg-duo-pic {
+      #mr-overview {
         display: flex;
-        margin-top: 3.6rem;
-        margin-bottom: 3.6rem;
-        .bgd-item {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          img{
-            padding: 1.2rem;
-            width: 90%;
+        flex-direction: column;
+        align-items: center;
+        ul{
+          margin: 5.2rem 0 0;
+          list-style: none;
+          padding-left: 0;
+          li{
+            font-family: 'Open Sans-R';
+            font-size: 1.7rem;
+            line-height: 2.8rem;
+            letter-spacing: 0.04rem;
+            color: #404040;
           }
-          label{
-            font-family: 'Charter-I';
-            font-weight: 100;
-            font-size: 1.4rem;;
-            color: #888888;
+        }
+        li::before{
+          content: "\2022";
+          color:  rgba(16, 81, 244, 0.3);
+          font-size: 2.4rem;
+          margin-right: 0.7rem;
+        }
+        p{
+          margin-top: 2.4rem;
+          width: 55%;
+        }
+      
+      }
+      #mr-sub4 {
+        display: flex;
+        flex-direction: column;
+        .mrs-row{
+          display: flex;
+          .mrs-item {
+            flex:1;
           }
         }
       }
       
     }
+   
     
     
   }
